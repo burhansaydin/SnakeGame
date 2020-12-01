c0712af6d196fc7bdcee2cfa2035c14bf561fc60
burhansaydin <burhasaydin@gmail.com>
2020-12-02 00:15:45 +0200

    edit

---
+++ main.py
@@ -0,0 +1,36 @@
+from turtle import Turtle, Screen
+import time
+from snake import Snake
+
+screen = Screen()
+screen.setup(width=600,height=600)
+screen.bgcolor("black")
+screen.title("My Snake Game!")
+screen.tracer(0)
+
+snake = Snake()
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.right, "Right")
+screen.onkey(snake.left, "Left")
+
+
+is_on=True
+while is_on:
+    screen.update()
+    time.sleep(0.1)
+
+    snake.move()
+
+
+
+
+
+
+
+
+
+
+
+screen.exitonclick()
